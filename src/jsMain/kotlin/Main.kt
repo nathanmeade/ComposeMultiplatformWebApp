5cd4536ecec2b9211d997c34e9e5a7102eebd49e
nathanmeade <nathan.r.meade@gmail.com>
2023-03-21 14:51:40 -0400

    Initial implementation created

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -6,24 +6,127 @@ import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
+    var fohEmployees: Int by mutableStateOf(0)
+    var bohEmployees: Int by mutableStateOf(0)
+    var fohEmployeeTipAmount: Double by mutableStateOf(0.0)
+    var bohEmployeeTipAmount: Double by mutableStateOf(0.0)
+    var totalTipsTextAreaString: String by mutableStateOf("")
+    var percentageString: String by mutableStateOf("")
 
     renderComposable(rootElementId = "root") {
-        Div({ style { padding(25.px) } }) {
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("Total tips:")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("$")
+            }
+            Span({ style { padding(15.px) } }) {
+                TextArea {
+                    this.onInput {
+                        totalTipsTextAreaString = it.value
+                    }
+                }
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("Number of FOH employees:")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
             Button(attrs = {
-                onClick { count -= 1 }
+                onClick { fohEmployees -= 1 }
             }) {
                 Text("-")
             }
 
             Span({ style { padding(15.px) } }) {
-                Text("$count")
+                Text("$fohEmployees")
             }
 
             Button(attrs = {
-                onClick { count += 1 }
+                onClick { fohEmployees += 1 }
             }) {
                 Text("+")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("Number of BOH employees:")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Button(attrs = {
+                onClick { bohEmployees -= 1 }
+            }) {
+                Text("-")
+            }
+
+            Span({ style { padding(15.px) } }) {
+                Text("$bohEmployees")
+            }
+
+            Button(attrs = {
+                onClick { bohEmployees += 1 }
+            }) {
+                Text("+")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("FOH tip percentage more:")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                TextArea {
+                    this.onInput {
+                        percentageString = it.value
+                    }
+                }
+            }
+            Span({ style { padding(15.px) } }) {
+                Text("%")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Button(attrs = {
+                onClick {
+                    val totalTipsFromString = totalTipsTextAreaString.toDouble()
+                    val percentageFromString = percentageString.toDouble() / 100
+                    bohEmployeeTipAmount =
+                        totalTipsFromString / (((percentageFromString + 1) * fohEmployees) + bohEmployees)
+                    fohEmployeeTipAmount = bohEmployeeTipAmount * (1 + percentageFromString)
+                }
+            }) {
+                Text("Calculate")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("FOH tips per employee:")
+            }
+            Span({ style { padding(0.px) } }) {
+                Text("$")
+            }
+            Span({ style { padding(0.px) } }) {
+                val text = fohEmployeeTipAmount.asDynamic().toFixed(2) as String
+                Text("$text")
+            }
+        }
+        Div({ style { padding(12.px) } }) {
+            Span({ style { padding(15.px) } }) {
+                Text("BOH tips per employee:")
+            }
+            Span({ style { padding(0.px) } }) {
+                Text("$")
+            }
+            Span({ style { padding(0.px) } }) {
+                val text = bohEmployeeTipAmount.asDynamic().toFixed(2) as String
+                Text("$text")
             }
         }
     }
